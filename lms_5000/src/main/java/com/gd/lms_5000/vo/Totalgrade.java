a6676340648733a0c81a3a1fb207f6caccf810d8
Man-E5 <dev2jones1@gmail.com>
2022-09-14 17:53:11 +0900

    [김현준] vo(week, totalgrade, test) 생성

---
+++ lms_5000/src/main/java/com/gd/lms_5000/vo/Totalgrade.java
@@ -0,0 +1,16 @@
+package com.gd.lms_5000.vo;
+
+import lombok.Data;
+
+@Data
+public class Totalgrade {
+	//필드
+	private int signNo;
+	private double totalGrade;
+	private int gradeTotal;
+	private int gradeRank;
+	private int gradeTest;
+	private int gradeAtt;
+	private int gradePaper;
+	
+}
---
+++ lms_5000/src/main/java/com/gd/lms_5000/vo/Week.java
@@ -0,0 +1,14 @@
+package com.gd.lms_5000.vo;
+
+import lombok.Data;
+
+@Data
+public class Week {
+	//필드
+	private int week;
+	private String startDate;
+	private String EndDate;	
+	private int lectureNo;
+	
+
+}
