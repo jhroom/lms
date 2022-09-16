c727f0cc6e518643116d673477171b2eb95b962a
tmdgus3173 <seungho6677@naver.com>
2022-09-16 12:45:46 +0900

    [신승현] ILmsNoticeService 작성완료

---
+++ lms_5000/src/main/java/com/gd/lms/service/ILmsNoticeService.java
@@ -0,0 +1,23 @@
+package com.gd.lms.service;
+
+import java.util.List;
+
+import com.gd.lms.vo.LmsNotice;
+
+public interface ILmsNoticeService {
+
+	// 리스트
+	List<LmsNotice> getLmsNoticeList();
+	
+	// 공지 추가 (사진도)
+	void addLmsAddNotice(LmsNotice lmsNotice, String path);
+	// 공지 삭제
+	int deleteLmsNotice(int lmsNoticeNo, String path);
+	// 공지 수정
+	void updateLmsNotice(LmsNotice lmsNotice, String path);
+	// 공지 상세보기
+	LmsNotice getLmsNoticeOne(int lmsNoticeNo);
+	
+	
+	
+}
