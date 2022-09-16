7e597db23e22f5a8bc8d6b3553632c98f5d56196
tmdgus3173 <seungho6677@naver.com>
2022-09-16 17:48:12 +0900

    [신승현] 리스트 구현 완료.

--- lms_5000/src/main/java/com/gd/lms/service/LmsNoticeService.java
+++ lms_5000/src/main/java/com/gd/lms/service/LmsNoticeService.java
@@ -1,5 +1,58 @@
 package com.gd.lms.service;
 
-public class LmsNoticeService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.lms.mapper.LmsNoticeMapper;
+import com.gd.lms.vo.LmsNotice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class LmsNoticeService implements ILmsNoticeService {
+	@Autowired LmsNoticeMapper lmsNoticeMapper;
+	
+	
+	// 공지 리스트
+	@Override
+	public List<LmsNotice> getLmsNoticeList() {
+		return lmsNoticeMapper.selectLmsNoticeList();
+	}
+	
+	
+	
+	
+	// 공지 등록
+	@Override
+	public void LmsAddNotice(LmsNotice lmsNotice, String path) {
+		//공지 등록 중..
+		
+	}
+
+	
+	
+	@Override
+	public int deleteLmsNotice(int lmsNoticeNo, String path) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateLmsNotice(LmsNotice lmsNotice, String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public LmsNotice getLmsNoticeOne(int lmsNoticeNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
