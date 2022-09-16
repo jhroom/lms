03a6812f788c33a410cb282f1e16be9914e4d1be
tmdgus3173 <seungho6677@naver.com>
2022-09-16 12:45:20 +0900

    [신승현] LMSNoticeMapper 작성 완료

--- lms_5000/src/main/java/com/gd/lms/mapper/LmsNoticeMapper.java
+++ lms_5000/src/main/java/com/gd/lms/mapper/LmsNoticeMapper.java
@@ -1,5 +1,22 @@
 package com.gd.lms.mapper;
 
-public class LmsNoticeMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gd.lms.vo.LmsNotice;
+
+@Mapper
+public interface LmsNoticeMapper {
+	//리스트
+	List<LmsNotice> selectLmsNoticeList();
+	//공지 추가
+	int LmsAddNotice(LmsNotice lmsNotice);
+	//공지 삭제
+	int deleteLmsNotice(int lmsNoticeNo);
+	//공지 수정
+	int updateLmsNotice(LmsNotice lmsNotice);
+	//공지 상세보기
+	LmsNotice selectLmsNoticeOne(int lmsNoticeNo);
+	
 }
