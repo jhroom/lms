77ade08a6de61e32f718f51db0a3d1ee705d07b1
tmdgus3173 <seungho6677@naver.com>
2022-09-28 12:43:35 +0900

    [신승현] 세부 내용 변경

--- lms_5000/src/main/java/com/gd/lms/mapper/MajorMapper.java
+++ lms_5000/src/main/java/com/gd/lms/mapper/MajorMapper.java
@@ -15,11 +15,11 @@ public interface MajorMapper {
 	// 학과 추가
 	public int addMajor(Major major);
 	
-	// 학과 삭제
-	public int deleteMajor(int majorNo);
-	
 	// 학과 수정
 	public int updateMajor(Major major);
+	
+	// 학과 삭제
+	public int deleteMajor(int majorNo);
 	
 	// 학과 상세보기
 	public Map<String, Object> selectMajorOne(int majorNo);
